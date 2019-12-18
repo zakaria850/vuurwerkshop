90eda278429bfcc6bcb9abdbb81258dd56423452
zakaria850 <58142346+zakaria850@users.noreply.github.com>
2019-12-18 17:59:12 +0100

    Add files via upload

---
+++ gehelepage.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang = nl>
+<head>
+<link href="vuurwerk.css" rel="stylesheet">
+<title>vuurwerkshop</title>
+</head>
+<body>
+    <div class"logo">
+        <div class="vierkant1"></div>
+        <div class="vierkant2"></div>
+        <div class="vierkant3"></div>
+        <div class="vierkant4"></div>
+        <div class="vierkant5"></div>
+        <div class="vierkant6"></div>
+    <div>
+	 <?php include'vuurwerkshop.php';
+ 
+  ?>
+<ul>
+    <a href="winkelwagen.html"><img src="winkelmand.jpg" title="mand" id="mand" alt="mand"><a></a> 
+    <li class="dropdown">
+        <a class="assortiment" href=""><h3>assortiment</h3></a>
+    <div class="dropdown-content">
+        <a href="vuurwerk.php">home</a>
+        <a href="sierpage.php">siervuurwerk</a>
+        <a href="knalpage.php">knalvuurwerk</a>
+        <a href="gehelepage.php">gehele assortiment</a>
+    </div>
+    </li>
+</ul>
+    <img src="vuurwerk.jpg" title="back" id="back" alt="back"> 
+</body> 
+</html>
